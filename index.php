c06d406711ddac0d0094e1def12de26e9945ca33
adrien88 <adrien.boilley@gmail.com>
2021-09-29 16:44:15 +0200

    wip

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Router;
+use App\game\Matches;
+use App\game\Player;
 use Frame\AutoloaderPSR4;
 
 ini_set('display_errors', '1');
@@ -8,4 +9,19 @@ session_start();
 
 include_once 'Frame/AutoloaderPSR4.php';
 new AutoloaderPSR4;
-new Router;
+// new Router;
+
+// $_SESSION['matches'] = [];
+
+$adrien = new Player('Adrien');
+$jojo = new Player('Jojo');
+$match = new Matches([$adrien, $jojo]);
+while (!$match->isClosed()) {
+    $set = $match->newSet();
+    if (null != $set) {
+        $score = $set->playSet();
+    }
+}
+echo 'Vaiqueur : ';
+
+var_dump($match);
