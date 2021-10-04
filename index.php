ad470977b2223c350753fb2afbfa347bb005895e
adrien88 <adrien.boilley@gmail.com>
2021-10-04 14:03:05 +0200

    fixes

--- index.php
+++ index.php
@@ -1,78 +1,8 @@
 <?php
 
-use App\game\Matchs;
-use App\game\Scoring;
-
-require_once('vendor/autoload.php');
 ini_set('display_errors', '1');
-
-// $players = ['Adrien', 'Saïd'];
-// $match = new Matchs(3);
-// do {
-//     $score = new Scoring($players, 11, 2);
-//     do {
-//         $score->increment($players[rand(0, 1)]);
-//     } while (!$score->isClosed());
-//     $match->playset($score);
-//     //
-// } while (!$match->isClosed());
-
-// foreach ($match->getSets() as $test)
-//     echo $test;
-
-
-/**
- * Affichage 
- */
-session_start();
-
-// unset($_SESSION['matches']);
-
-/**
- * load match
- */
-if (isset($_SESSION['matches']) and !empty($_SESSION['matches']))
-    [$match, $score, $players] = end($_SESSION['matches']);
-
-
-$page = (isset($_GET['page'])) ? $_GET['page'] : 'new';
-switch ($page) {
-    case 'new':
-        if (isset($_POST['player1']) && isset($_POST['player1'])) {
-            $match = new Matchs(3);
-            $players = [$_POST['player1'], $_POST['player2']];
-            $score = new Scoring($players, 11, 2);
-            $page = 'playing';
-        }
-        break;
-
-    case 'playing':
-        if (isset($_GET['player'])) {
-            if (!$match->isClosed()) {
-                if (!$score->isClosed()) {
-                    $score->increment($_GET['player']);
-                } else {
-                    $match->playset($score);
-                    $score = new Scoring($players, 11, 2);
-                }
-            } else {
-                $page = 'scores';
-            }
-        }
-        break;
-
-    case 'scores':
-        break;
-}
-
-/**
- * save statement match
- */
-if (isset($match) && isset($score) && isset($players))
-    $_SESSION['matches'][] = [$match, $score, $players];
-
-
-// $page = 'playing';
+require_once('vendor/autoload.php');
+include_once 'App/src/includes/main.php';
 
 ?>
 
@@ -84,7 +14,17 @@ if (isset($match) && isset($score) && isset($players))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="App/src/assets/css/style.css">
+
     <title>PingCounter</title>
+
+    <!-- <script type="module" defer>
+        import {
+            Game
+        } from "./App/src/assets/js/Game.js";
+        customElements.define('tenis-game', Game, {});
+        document.body.appendChild(new Game);
+    </script> -->
+
 </head>
 
 <body>
@@ -116,13 +56,13 @@ if (isset($match) && isset($score) && isset($players))
 
         <!-- Playing -->
         <?php if ('playing' == $page) : ?>
-            <div class="left">
-                <div id="player1"><?= $score->getScore($players[0]) ?></div>
-                <a class="button" href='?page=playing&player=<?= $players[0] ?>'><?= $players[0] ?></a>
+            <div id="playerLeft" class="left">
+                <div id=""><?= $score->getScore($players[0]) ?> </div>
+                <a href="?page=playing&player=<?= $players[0] ?>"><?= $players[0] ?></a>
             </div>
-            <div class="right">
-                <div id="player2"><?= $score->getScore($players[1]) ?></div>
-                <a class="button" href='?page=playing&player=<?= $players[1] ?>'><?= $players[1] ?></a>
+            <div id="playerRight" class="right">
+                <div id=""><?= $score->getScore($players[1]) ?></div>
+                <a href="?page=playing&player=<?= $players[1] ?>"><?= $players[1] ?></a>
             </div>
         <?php endif ?>
 
